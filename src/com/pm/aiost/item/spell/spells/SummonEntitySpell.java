4884c799b7021e1711846cd36d0059d0dcc3d5b6
limaxp <Maximilian.paar@gmx.at>
2024-05-30 13:20:45 +0200

    Removed OwnableEntity

--- src/com/pm/aiost/item/spell/spells/SummonEntitySpell.java
+++ src/com/pm/aiost/item/spell/spells/SummonEntitySpell.java
@@ -8,27 +8,27 @@ import org.bukkit.Particle;
 import org.bukkit.entity.LivingEntity;
 
 import com.pm.aiost.entity.AiostEntityTypes;
-import com.pm.aiost.entity.OwnableEntity;
 import com.pm.aiost.item.spell.Spell;
 import com.pm.aiost.player.ServerPlayer;
 
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 
 public class SummonEntitySpell extends Spell {
 
 	protected final static int RANGE = 20;
 	protected final static int DEFAULT_BOUND_TIME = 1200;
 
-	private final EntityType<? extends OwnableEntity> entityType;
+	private final EntityType<? extends Mob> entityType;
 	private final int duration;
 
-	public SummonEntitySpell(String name, int cooldown, double cost, EntityType<? extends OwnableEntity> entityType,
+	public SummonEntitySpell(String name, int cooldown, double cost, EntityType<? extends Mob> entityType,
 			int duration) {
 		this(name, name, cooldown, cost, entityType, duration);
 	}
 
 	public SummonEntitySpell(String name, String displayName, int cooldown, double cost,
-			EntityType<? extends OwnableEntity> entityType, int duration) {
+			EntityType<? extends Mob> entityType, int duration) {
 		super(name, displayName, cooldown, cost);
 		this.entityType = entityType;
 		this.duration = duration;
@@ -38,10 +38,10 @@ public class SummonEntitySpell extends Spell {
 	public void cast(LivingEntity entity, int i) {
 		Location target = entity.getTargetBlock((Set<Material>) null, RANGE).getLocation().clone().add(0, 1, 0);
 		if (target != null) {
-			OwnableEntity ownable = ((OwnableEntity) spawnEntity(target));
-			ownable.setOwner(entity);
-			ownable.setBoundTime(duration);
-			ownable.getBukkitEntity().setGlowing(true);
+			Mob ownable = spawnEntity(target);
+//			ownable.setOwner(entity);
+//			ownable.setBoundTime(duration);
+//			ownable.getBukkitEntity().setGlowing(true);
 		}
 	}
 
@@ -50,21 +50,21 @@ public class SummonEntitySpell extends Spell {
 		Location target = serverPlayer.player.getTargetBlock((Set<Material>) null, RANGE).getLocation().clone().add(0,
 				1, 0);
 		if (target != null) {
-			OwnableEntity ownable = ((OwnableEntity) spawnEntity(target));
-			ownable.setOwner(serverPlayer.player);
-			ownable.setBoundTime(duration);
-			ownable.getBukkitEntity().setGlowing(true);
+			Mob ownable = spawnEntity(target);
+//			ownable.setOwner(serverPlayer.player);
+//			ownable.setBoundTime(duration);
+//			ownable.getBukkitEntity().setGlowing(true);
 			return true;
 		}
 		return false;
 	}
 
-	protected OwnableEntity spawnEntity(Location loc) {
+	protected Mob spawnEntity(Location loc) {
 		loc.getWorld().spawnParticle(Particle.PORTAL, loc, 20, 0.5F, 0.5F, 0.5F, 0.1F, null, false);
-		return (OwnableEntity) AiostEntityTypes.spawnEntity(entityType, loc);
+		return (Mob) AiostEntityTypes.spawnEntity(entityType, loc);
 	}
 
-	public EntityType<? extends OwnableEntity> getEntityType() {
+	public EntityType<? extends Mob> getEntityType() {
 		return entityType;
 	}
 
